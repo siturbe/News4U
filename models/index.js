d3d7bbaba274c5e8fc5f71f4cfeaed77d1e67f65
ChilorioX <santiago.iturbe@gmail.com>
2019-12-17 09:57:47 -0600

    New code base, was stuck on why prior code wasn't working, so started new one in more organized manner

--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
+// Exporting an object containing all of our models
+
 module.exports = {
-    Comment: require('./Comment')
-}
+  Article: require("./Article"),
+  Note: require("./Note")
+};
--- server.js
+++ server.js
@@ -1,138 +1,131 @@
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
-var router = express.Router();
-var assert = require('assert');
-// var objectID = require('mongojs').ObjectID;
 
-var PORT = 3000;
-// Require axios and cheerio. This makes the scraping possible
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-app.use(express.urlencoded({extended: true}));
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-//Set handlebars as the default templating engine:
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
-
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData", "discussions"];
-
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
-
-var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/mongoHeadlines";
-
+// Connect to the Mongo DB
+var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/News4U";
 mongoose.connect(MONGODB_URL, {useNewUrlParser: true}, { useUnifiedTopology: true });
 
-// Main route (simple Hello World Message)
-app.get("/testJSON", function(req, res) {
-    db.scrapedData.find({}, function(error, found) {
-        // Throw any errors to the console
-        if (error) {
-          console.log(error);
-        }
-        // If there are no errors, send the data to the browser as json
-        else {
-          res.json(found);
-        }
-      });
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // Make a request via axios for the news section of `ycombinator`
+  axios.get("https://www.newsweek.com/newsfeed").then(function(response) {
+    // Load the html body from axios into cheerio
+    let $ = cheerio.load(response.data);
+    // For each element with a "title" class
+    $(".inner").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      let title = $(element).children("h3").children("a").text();
+      let link = "https://www.newsweek.com" + $(element).children("h3").children("a").attr("href");
+      let summary = $(element).children(".summary").text();
+      let saved = false;
+      let note;
+
+      // If this found element had both a title and a link
+      if (title && link && summary) {
+        // Insert the data in the scrapedData db
+        db.Article.create({
+          title: title,
+          link: link,
+          summary: summary,
+          saved: saved,
+          note: note
+        },
+        function(err, dbArticle) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log("Scraped data inserted");
+          }
+        });
+      }
+    });
+  });
+  // Send a "Scrape Complete" message to the browser
+  console.log('Referesh Worked');
 });
-  
- 
 
-  //Retrieve Data from db
-  app.get("/", function(req, res) {
-    // Find all results from the scrapedData collection in the db
-    db.scrapedData.find({}).sort({'_id': -1 }).toArray(function(error, found) {
-      
-      // Throw any errors to the console
-      if (error) {
-        console.log(error);
-      }
-      // If there are no errors, send the data to the browser as json
-      else {
-        res.render("index", {stories: found});
-      }
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
-  });
+});
 
- //Update for a new comment
- app.post('/update-comment', function(req, res,) {
-   console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-   console.log("ID: " + req.body.id);
-   console.log("Author: " + req.body.author);
-   console.group("Comment: " + req.body.content);
-    let objectId = "Object("+ req.body.id +")";
-    console.log(objectId);
-
-   db.scrapedData.update({"_id": objectId }
-    , {
-     $push: {
-       "comments": {author: req.body.author, content: req.body.content}
-        }
-     }, function(err, post) {
-      console.log("Updated with Comment");
-   });
- })
-
-
-  
-  // Scrape data from one site and place it into the mongodb db
-  app.get("/api/scrape", function(req, res) {
-    // Make a request via axios for the news section of `ycombinator`
-    axios.get("https://www.newsweek.com/newsfeed").then(function(response) {
-      // Load the html body from axios into cheerio
-      let $ = cheerio.load(response.data);
-      // For each element with a "title" class
-      $(".inner").each(function(i, element) {
-        // Save the text and href of each link enclosed in the current element
-        let title = $(element).children("h3").children("a").text();
-        let link = "https://www.newsweek.com" + $(element).children("h3").children("a").attr("href");
-        let summary = $(element).children(".summary").text();
-        let comments = {};
-  
-        // If this found element had both a title and a link
-        if (title && link && summary) {
-          // Insert the data in the scrapedData db
-          db.scrapedData.insert({
-            title: title,
-            link: link,
-            summary: summary,
-            comments: comments
-          },
-          function(err, inserted) {
-            if (err) {
-              // Log the error if one is encountered during the query
-              console.log(err);
-            }
-            else {
-              // Otherwise, log the inserted data
-              console.log("Scraped data inserted");
-            }
-          });
-        }
-      });
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
-  
-    // Send a "Scrape Complete" message to the browser
-    console.log('Referesh Worked');
-  });
-  
-  
-  // Listen on port 3000
-  app.listen(PORT, function() {
-    console.log("App running on port " + PORT);
-  });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
