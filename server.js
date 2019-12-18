b100a56e437a7bad11ccdc935e9e7ca5666b2b78
ChilorioX <santiago.iturbe@gmail.com>
2019-12-17 23:39:17 -0600

    Added ability to delete comments, and cleaned up some files of old texted-out code

--- server.js
+++ server.js
@@ -89,19 +89,6 @@ app.get("/", function(req, res) {
     });
 });
 
-// Route for getting all Articles from the db
-// app.get("/articlesOLD", function(req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
@@ -164,6 +151,7 @@ app.get("/savedArticles", function(req, res){
     });
 });
 
+//For deleting articles
 app.post("/delete/:id", function (req,res){
   db.Article.deleteOne({_id: req.params.id})
   .then(function(dbArticle){
@@ -174,6 +162,16 @@ app.post("/delete/:id", function (req,res){
   })
 })
 
+//For deleting comments
+app.post("/delete-comment/:id", function (req,res){
+  db.Note.deleteOne({_id: req.params.id})
+  .then(function(dbNote){
+    res.json(dbNote);
+  })
+  .catch(function(err){
+    res.json(err);
+  })
+})
 
 // Route to generate 
 app.get("/comments/:id",function(req, res){
